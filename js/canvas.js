dafe3fc0fb3e15c7aeed3280d9d1ab766778a5e9
Quigley <quigley@resurnate.com>
2023-07-03 22:42:15 -0500

    Knocked out refactoring captions

--- js/canvas.js
+++ js/canvas.js
@@ -9,9 +9,17 @@ const CANVAS_ROWS = 2;
 const PANEL_PADDING = 10;
 const PANEL_WIDTH = 630;
 const PANEL_HEIGHT = 630;
+
+//
+// C A P T I O N S
+//
+
 const CAPTION_WIDTH = (PANEL_WIDTH - PANEL_PADDING) / 2;
 const CAPTION_HEIGHT = 90;
 const CAPTION_BORDER_STYLE = '#666666';
+const CAPTION_BOX_COLORS = [
+    '#FFFF88', '#FFFFCC', '#CCE5FF','#DAE8FC'
+];
 const CAPTION_FONT_SIZE = 27;
 const CAPTION_FONT = CAPTION_FONT_SIZE+'px '+FONT_FAMILY_COMIC;
 const CAPTION_OFFSETS = {
@@ -19,14 +27,14 @@ const CAPTION_OFFSETS = {
 };
 
 /**
- * Parse all panel caption input and return object matching position.
+ * Parse all input and return caption matching position.
  * @param icps All caption input
- * @param p    Caption position
+ * @param i    Caption position
  */
-function parsePanelCaption(icps,p) {
+function parsePanelCaption(icps,i) {
     var r;
     for (var icp of icps) {
-        if ((icp.position - 1) === p) {
+        if ((icp.position - 1) === i) {
             r = icp;
             break;
         }
@@ -35,53 +43,70 @@ function parsePanelCaption(icps,p) {
 }
 
 /**
- * Prepare box from panel caption input.
+ * Prepare all captions in panel.
  * @param p    Prepared panel
- * @param icps All caption input
- * @param i    Caption index
+ * @param icps All panel caption input
  */
-function preparePanelCaptionBox(p,icps,i) {
+function preparePanelCaptions(p, icps) {
+    let r = [];
+    for (var i = 0; i < 4; i++) {
+        let icp = parsePanelCaption(icps,i);
+        r.push(preparePanelCaption(p,icp,i));
+    }
+    return r;
+}
+
+/**
+ * Prepare caption box and text from panel input.
+ * @param p    Prepared panel
+ * @param icp  Caption input
+ * @param i    Caption position index
+ */
+function preparePanelCaption(p,icp,i) {
+    let rb = preparePanelCaptionBox(p,i);
+    var rt = [];
+    if (icp !== undefined) { // Has caption
+        rt = preparePanelCaptionText(icp);
+    }
+    return {
+        b : rb,
+        t : rt
+    };
+}
+
+/**
+ * Prepare caption box from panel input.
+ * @param p    Prepared panel
+ * @param i    Caption position index
+ */
+function preparePanelCaptionBox(p,i) {
     var rx = p.x;
     var ry = p.y;
-    var rc = "#FFFFFF";
-    var rf = "27px Comic Sans MS";
-    var rt = [];
+    var rc = CAPTION_BOX_COLORS[i];
     if (i === 0) {        // Upper-left
         rx = p.x;
         ry = p.y;
-        rc = "#FFFF88";
-        rf = 'italic bold ' + rf;
-        rt = prepareCaption(icps,i);
     } else if (i === 1) { // Upper-right
         rx = p.x + p.p + CAPTION_WIDTH;
         ry = p.y;
-        rc = "#FFFFCC";
-        rf = 'italic bold ' + rf;
-        rt = prepareCaption(icps,i);
     } else if (i === 2) { // Lower-left
         rx = p.x;
         ry = p.y + p.h - CAPTION_HEIGHT;
-        rc = "#CCE5FF";
-        rt = prepareCaption(icps,i);
     } else {                // Lower-right
         rx = p.x + p.p + CAPTION_WIDTH;
         ry = p.y + p.h - CAPTION_HEIGHT;
-        rc = "#DAE8FC";
-        rt = prepareCaption(icps,i);
     }
     return {
         w: CAPTION_WIDTH,
         h: CAPTION_HEIGHT,
         x: rx,
         y: ry,
-        c: rc,
-        f: rf,
-        t: rt
+        c: rc
     };
 }
 
 /**
- * Prepare all text lines from panel caption input.
+ * Prepare all caption text lines from panel input.
  * @param icp Caption input
  */
 function preparePanelCaptionText(icp) {
@@ -96,8 +121,8 @@ function preparePanelCaptionText(icp) {
 }
 
 /**
- * Prepare single text line from panel caption input.
- * @param p Caption input position
+ * Prepare single caption text line from panel input.
+ * @param p Caption position
  * @param l Line of text
  * @param i Line index
  */
@@ -149,22 +174,22 @@ function drawPanelCaption(c,cp) {
  * @param cp Prepared caption
  */
 function drawPanelCaptionBox(c,cp) {
-    c.cc.fillStyle = cp.c;
-    c.cc.fillRect(cp.x,cp.y,cp.w,cp.h);
+    c.cc.fillStyle = cp.b.c;
+    c.cc.fillRect(cp.b.x,cp.b.y,cp.b.w,cp.b.h);
     c.cc.strokeStyle = CAPTION_BORDER_STYLE;
-    c.cc.strokeRect(cp.x,cp.y,cp.w,cp.h);
+    c.cc.strokeRect(cp.b.x,cp.b.y,cp.b.w,cp.b.h);
 }
 
 /**
- * Draw all text lines in panel caption.
+ * Draw caption text in panel.
  * @param c  Prepared canvas
  * @param cp Prepared caption
  */
 function drawPanelCaptionText(c,cp) {
 
     // Offset
-    let x = cp.x + (cp.w / 2) + CAPTION_OFFSETS.t.x;
-    var y = cp.y + (cp.h / 2) - (Math.floor(cp.t.length / 2) * CAPTION_FONT_SIZE);
+    let x = cp.b.x + (cp.b.w / 2) + CAPTION_OFFSETS.t.x;
+    var y = cp.b.y + (cp.b.h / 2) - (Math.floor(cp.t.length / 2) * CAPTION_FONT_SIZE);
     // Recenter if even number of lines
     if ((cp.t.length % 2) === 0) { y += (CAPTION_FONT_SIZE / 2); }
     y += CAPTION_OFFSETS.t.y;
@@ -179,6 +204,10 @@ function drawPanelCaptionText(c,cp) {
     drawPanelText(c,t);
 
 }
+
+//
+// M I S C E L L A N E O U S
+//
 
 /**
  * Draw all text lines in panel bubble or caption.
